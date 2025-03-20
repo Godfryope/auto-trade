a82736fe52cde823de3dc5218d88c6efb9d9ed2b
godfryope <elolengodfrey@gmail.com>
2025-03-20 14:58:45 +0100

    pumpportal api - subscribeNewToken

--- index.js
+++ index.js
@@ -275,7 +275,6 @@ app.put('/api/user/:telegramId', async (req, res) => {
 
 // WebSocket setup for Raydium liquidity events
 const ws = new WebSocket('wss://pumpportal.fun/api/data');
-const socket = io();
 
 ws.on('open', function open() {
   // Subscribing to Raydium liquidity events
